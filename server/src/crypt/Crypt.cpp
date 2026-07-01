a63036b86540fcad2d006d5724c62f560ab9188d
I767435 <erikkoston@mailfence.com>
2026-07-01 08:29:51 +0200

    something

--- server/src/crypt/Crypt.cpp
+++ server/src/crypt/Crypt.cpp
@@ -1,8 +1,21 @@
 #include "Crypt.hpp"
+#include <iostream>
+#include <openssl/evp.h>
 #include <string>
+
 namespace crypt {
-  std::string base64Decode(std::string) {
-    std::string decoded = "";
-    return decoded;
-  };
-}
+
+std::string base64Decode(std::string in) {
+  unsigned char* decoded;
+  std::vector<unsigned char> buffer(in.begin(), in.end());
+  unsigned char* bufferPtr = buffer.data();
+  int len = 0;
+  int resLen = EVP_DecodeBlock(decoded, bufferPtr, len);
+  if (resLen == -1) {
+    std::cerr << "failed to decode block" << std::endl;
+    return NULL;
+  }
+  return "";
+};
+
+} // namespace crypt
